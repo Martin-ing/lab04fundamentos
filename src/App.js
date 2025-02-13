650a68e8b1dfded8406f835946b3817947ee266a
Martin-ing <23002494@galileo.edu>
2025-02-12 21:37:00 -0600

    cambiar frase random

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Container, Button, Card } from "react-bootstrap";
 class QuoteGenerator extends Component {
   constructor(props) {
     super(props);
-    this.state = {frase_actual: 0,frases: []};
+    this.state = { frase_actual: 0, frases: ["a", "b", "c"] };
     this.cambiar_frase = this.cambiar_frase.bind(this);
   }
 
@@ -13,7 +13,12 @@ class QuoteGenerator extends Component {
     this.interval = setInterval(this.cambiar_frase, 5000);
   }
 
-  cambiar_frase() {}
+  cambiar_frase() {
+    const { frases, frase_actual } = this.state;
+    let nueva_frase = Math.floor(Math.random() * frases.length);
+
+    this.setState({ frase_actual: nueva_frase });
+  }
 
   render() {
     return (
@@ -23,9 +28,7 @@ class QuoteGenerator extends Component {
           <div className="display-4 text-dark">
             {this.state.frases[this.state.frase_actual]}
           </div>
-          <div className="text-muted mt-2">
-            autor
-          </div>
+          <div className="text-muted mt-2">autor</div>
         </Card>
       </Container>
     );
