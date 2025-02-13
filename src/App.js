430be791e36209e3da4061f7d77c922ae93acdf5
Martin-ing <23002494@galileo.edu>
2025-02-12 21:22:20 -0600

    iniciando interfaz

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button, Card } from "react-bootstrap";
+
+class QuoteGenerator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {frase_actual: 0,frases: []};
+    this.cambiar_frase = this.cambiar_frase.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.cambiar_frase, 5000);
+  }
+
+  cambiar_frase() {}
+
+  render() {
+    return (
+      <Container className="text-center mt-5">
+        <h2 className="mb-3 text-primary">Quote Generator</h2>
+        <Card className="shadow-lg p-3">
+          <div className="display-4 text-dark">
+            {this.state.frases[this.state.frase_actual]}
+          </div>
+          <div className="text-muted mt-2">
+            autor
+          </div>
+        </Card>
+      </Container>
+    );
+  }
+}
+
+export default QuoteGenerator;
