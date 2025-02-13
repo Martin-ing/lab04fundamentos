9262a0b382e6efb7cbe1ec4c26575701642c28b6
Martin-ing <23002494@galileo.edu>
2025-02-12 22:42:20 -0600

    lab terminado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Button, Card } from "react-bootstrap";
+import "./styles.css";
 
 class QuoteGenerator extends Component {
   constructor(props) {
     super(props);
     this.state = {
       frase_actual: 0,
-      frases: ["a", "b", "c"],
-      autores: ["-a", "-b", "-c"],
+      frases: [
+        "No hay hombre mas infeliz que el que nunca atraviesa adversidades",
+        "Las dificultades fortalecen la mente como el trabajo al cuerpo",
+        "El hombre de verdad se revela en tiempos dificiles",
+        "La mala suerte prueba a los hombres valientes",
+        "Recibe los eventos de cualquier forma, es el camino a la paz",
+        "El dolor no es insoportable siempre y cuando no lo empeores con tu mente",
+      ],
+      autores: [
+        "-Seneca",
+        "-Seneca",
+        "-Epictetus",
+        "-Seneca",
+        "-Epictetus",
+        "-Marcus Aurelius",
+      ],
     };
     this.cambiar_frase = this.cambiar_frase.bind(this);
   }
@@ -27,7 +42,7 @@ class QuoteGenerator extends Component {
   render() {
     return (
       <Container className="text-center mt-5">
-        <h2 className="mb-3 text-primary">Quote Generator</h2>
+        <h2 className="mb-3 text-custom">Quote Generator</h2>
         <Card className="shadow-lg p-3">
           <div className="display-4 text-dark">
             {this.state.frases[this.state.frase_actual]}
@@ -38,7 +53,7 @@ class QuoteGenerator extends Component {
           <Button
             onClick={this.cambiar_frase}
             variant="primary"
-            className="mt-3"
+            className="mt-3 button-custom"
           >
             New Quote
           </Button>
